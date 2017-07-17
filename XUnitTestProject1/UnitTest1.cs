2380b25f041dad27adf5f8acce68155888934bf7
penartur <penartur@users.noreply.github.com>
2017-07-17 09:59:01 +0300

    Added XUnit test project

---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace XUnitTestProject1
+{
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
